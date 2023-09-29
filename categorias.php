53ce70d5d8aac0857675741eb0f08fd4c1c0b045
GemaCastañeda <146403711+geeems@users.noreply.github.com>
2023-09-28 20:44:54 -0600

    Categorias_nuevo

--- categorias.php
+++ categorias.php
@@ -4,11 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<!--Boostrap-->   
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+<!--Boostrap--> 
+<link rel="stylesheet" href="bootstrap/css/estilos.css">
 </head>
-<body>
-    <?php include 'index.php';?>
-    <br>
 
-        <center><h1>CATEGORIAS</h1></center>
+<body>
+
+<?php include 'templates/navbar/navbar.html';?>
+
+    <section id="banner">
+        <div id="outer">
+          <div id="hero">
+            <h2 style="color: white;">CATEGORIAS</h2>
+          </div>
+        </div>
+      </section>
+          
+
+      
+
+
+
 </body>
 </html>
