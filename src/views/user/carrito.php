f5ee0ad9d539ed6e797876041f0a3c8e81552246
DiegoIvC <109551862+DiegoIvC@users.noreply.github.com>
2023-10-01 14:59:11 -0600

    Carrito-link

---
+++ src/views/user/carrito.php
@@ -0,0 +1,14 @@
+<?php include '../../../templates/navbar/navbar.html';?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    
+</head>
+<body>
+    
+</body>
+</html>
