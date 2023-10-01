687dfd2bfba2e0d521c3402afb52ce743defd792
DiegoIvC <109551862+DiegoIvC@users.noreply.github.com>
2023-10-01 00:30:52 -0600

    boton back

--- src/views/user/productos.php
+++ src/views/user/productos.php
@@ -29,8 +29,8 @@
         }
         .productotop
         {
-            padding-top: 25% !important;
-            padding-right: 5% !important;
+            padding-top: 10% !important;
+            padding-right: 10% !important;
         }
         .productobott
         {
@@ -116,16 +116,18 @@
             border-radius: 40px;
         }
         .cont-back{
+            position: relative;
             width: 100%;
             height: 150px;
         }
-        .back{
-            
+        .icono{
+            margin-top: 2em;
+            margin-left:2em;
             width: 50px;
             height: 50px;
             color: #202124;
         }
-        svg:hover{
+        .icono:hover{
             
             width: 60px;
             height: 60px;
@@ -147,11 +149,14 @@
   
 
         <div class="row">
-            <a href="../../../index.php" class="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left back" viewBox="0 0 16 16">
+            <div class="cont-back">
+                <a href="../../../index.php" class="">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left icono" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                 </svg>  
             </a>
+            </div>
+            
             <div class="col-sm-12 col-md-6 ">
                 <div class="productotop">
                 <div id="carouselExampleControlsNoTouching" class="carousel carousel-dark slide" data-bs-touch="false">
