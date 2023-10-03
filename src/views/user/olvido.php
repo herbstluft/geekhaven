eeeeb475b5f6a0dcf2ad2ca23bfcc9675ee8fe98
miguelcastrooo <109358986+miguelcastrooo@users.noreply.github.com>
2023-10-02 20:46:10 -0600

    login

---
+++ src/views/user/olvido.php
@@ -0,0 +1,92 @@
+<?php
+include("../../../templates/navbar/navbar.html");
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>
+  <style>
+
+            a{
+            color: #333;
+            }
+            a:hover {
+              color: #FF0000; /* Rojo (#FF0000) cuando se pasa el mouse */
+            }
+        .formulario {
+            border: 2px solid #ccc;
+            padding: 20px;
+            border-radius: 10px;
+            max-width: 400px;
+            max-height: ;
+            margin: 0 auto;
+            background: #F4F4F4;
+        }
+
+            #nombre {
+                margin-right: 10px; /* Agrega espacio a la derecha del campo "nombre" */
+            }
+
+            #apellidos {
+                margin-left: 10px; /* Agrega espacio a la izquierda del campo "apellidos" */
+            }
+
+        .form-group-horizontal {
+            display: flex;
+            justify-content: space-between;
+        }
+    </style>
+</head>
+<body>
+    <br><br>
+    <div class="container mt-5">
+        <div class="formulario">
+            <br><br>
+            <center><h2><strong>Recuperar contraseña</strong></h2></center>
+            <br>
+            <form>
+                <div class="mb-3">
+                    <input type="email" class="form-control" id="correo" placeholder="Tu correo electrónico">
+                </div>
+                    <br>               
+                    <center>  <button type="submit" class="btn bg-danger text-white btn-lg" style="width: 350px;">Recuperar</button></center>
+                        <br>
+                       <center> <a href="login.php">Volver al inicio de Sesion</a></center>
+
+                </div>
+
+  </main>
+  <div class="container">
+  <?php
+  
+        include("../../../templates/footer.html");
+    ?>
+    </div>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+
+</body>
+
+</html>
---
+++ src/views/user/registrouser.php
@@ -0,0 +1,106 @@
+<?php
+    use MyApp\data\Database;
+    require("../../../vendor/autoload.php");
+    $db = new Database;
+    
+    $resultado = "select * from usuarios";
+
+    $guardar = $db->seleccionarDatos($resultado);
+    print_r($guardar);
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rA6K/Q1vxgFm8z4lDf3m6JXVnF5SRhdpv4LwB5FtY2O5f5xSTF+to8SGz4SOGp2z9v" crossorigin="anonymous">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>Formulario de Registro</title>
+    <style>
+        a{
+            color: #333;
+
+        }
+        a:hover {
+              color: #FF0000; /* Rojo (#FF0000) cuando se pasa el mouse */
+            }
+        .formulario {
+            border: 2px solid #ccc;
+            padding: 20px;
+            border-radius: 10px;
+            max-width: 530px;
+            margin: 0 auto;
+            background: #F4F4F4;
+        }
+
+            #nombre {
+                margin-right: 2px; /* Agrega espacio a la derecha del campo "nombre" */
+            }
+
+            #apellidos {
+                margin-left: 2px; /* Agrega espacio a la izquierda del campo "apellidos" */
+            }
+
+        .form-group-horizontal {
+            display: flex;
+            justify-content: space-between;
+        }
+    </style>
+</head>
+<body>
+<?php
+include("../../../templates/navbar/navbar.html");
+?>
+    <br><br>
+    <div class="container mt-5">
+        <div class="formulario">
+            <center><h2>Registro</h2></center>
+            <form method="POST" action="registro.php">
+                <div class="mb-3">
+                    <label for="correo" class="form-label">Correo Electrónico</label>
+                    <input type="email" class="form-control" name="correo" placeholder="Tu correo electrónico">
+                </div>
+                <div class="mb-3">
+                    <label for="contrasena" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" name="contrasena" placeholder="Tu contraseña">
+                </div>
+                <div class="form-group-horizontal">
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" placeholder="Tu nombre">
+                    </div>
+                    <div class="mb-3">
+                        <label for="apellidos" class="form-label">Apellidos</label>
+                        <input type="text" class="form-control" name="apellidos" placeholder="Tus apellidos">
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="telefono" class="form-label">Teléfono</label>
+                    <input type="tel" class="form-control" name="telefono" placeholder="Tu teléfono">
+                </div>
+              <center>  <button type="submit" class="btn bg-danger text-white btn-lg" style="width: 350px;">Registrarse</button></center>
+              <br>
+              <center><a href="login.php">¿Ya tienes cuenta?</a></center>
+            </form>
+        </div>
+    </div>
+    <div class="container">
+  <?php
+  
+        include("../../../templates/footer.html");
+    ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+</html>
