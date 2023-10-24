13c2f50ec84b8723e9c4594ca1337236fab19a67
GemaCastañeda <146403711+geeems@users.noreply.github.com>
2023-10-23 20:37:46 -0600

    Create cat.php

---
+++ src/views/user/cat.php
@@ -0,0 +1,73 @@
+<?php
+    use MyApp\data\Database;
+    require("../../../vendor/autoload.php");
+    $db = new Database;
+
+    ?>
+    <!doctype html>
+    <html lang="en">
+    
+    <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Modernize Free</title>
+  <link rel="shortcut icon" type="image/png" href="/geekhaven/src/views/admin/assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="/geekhaven/src/views/admin/assets/css/styles.min.css" />
+  <link rel="stylesheet" href="/geekhaven/bootstrap/css/estilos.css" />
+  
+</head>
+    <body>
+    
+    <?php include('../../../templates/navbar_user.php') ?>
+
+    <div class="container-fluid">
+    
+<center><h1>Categorias</h1></center>
+<br>
+   
+        
+
+<br><br>       
+
+        
+        
+
+        <div class="row">
+           
+                        <div class="col-sm-6 col-xl-3">
+                            
+                        <div class="scroll-appear">
+                            <div class="card overflow-hidden rounded-2">
+                            <div class="position-relative">
+    <a href="/geekhaven/src/views/user/productos.php?id="><img src="/geekhaven/src/views/admin/assets/images/products/s4.jpg" class="card-img-top rounded-0" alt="..."></a>
+    <a href="javascript:void(0)" class="bg-success rounded-circle p-2 text-white d-inline-flex position-absolute bottom-0 end-0 mb-n3 me-3" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Add To Cart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"></path>
+</svg>
+    </a>                      
+  </div>
+  <div class="card-body pt-3 p-4">
+        <div style="width:100%;">
+        <h6 class="fw-semibold fs-4 text-truncate"> dfrfr </h6>
+        </div>
+    <div class="d-flex align-items-center justify-content-between">
+      <h6 class="fw-semibold fs-4 mb-0">frjjri</h6>
+      <ul class="list-unstyled d-flex align-items-center mb-0">
+      vfvfv
+    </div>
+  </div>
+                            </div>
+                            </div>
+                            </a>
+                        </div>
+                  
+   
+            
+        </div>
+    </div>
+
+      </div>
+      <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+      <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="../assets/js/sidebarmenu.js"></script>
+      <script src="../assets/js/app.min.js"></script>
+      <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
