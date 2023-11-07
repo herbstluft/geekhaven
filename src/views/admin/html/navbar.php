3f937bc4c0c6e58223db94a9321b91fc9da9ba93
miguelcastrooo <109358986+miguelcastrooo@users.noreply.github.com>
2023-11-06 20:19:55 -0600

    modificaciones

--- src/views/admin/html/navbar.php
+++ src/views/admin/html/navbar.php
@@ -65,19 +65,19 @@ $img_ad=$img_admin['imagen'];
             </li>
 
             <li class="sidebar-item">
-              <a class="sidebar-link" href="html/agregar_producto.php" aria-expanded="false">
+              <a class="sidebar-link" href="/geekhaven/src/views/admin/html/mod.php" aria-expanded="false">
                 <span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-file-minus" viewBox="0 0 16 16">
   <path d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
   <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
 </svg>
                 </span>
-                <span  class="hide-menu">Eliminar producto</span>
+                <span  class="hide-menu">Eliminar o Modificar producto</span>
               </a>
             </li>
 
             <li class="sidebar-item">
-              <a class="sidebar-link" href="html/agregar_producto.php" aria-expanded="false">
+              <a class="sidebar-link" href="mod.php" aria-expanded="false">
                 <span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
