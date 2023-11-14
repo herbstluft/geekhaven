d3830a7a101dcb8c9da426d18b21995f3828b5bd
DiegoIvC <diego.cisneros.dp@gmail.com>
2023-11-13 20:52:24 -0600

    ASD

--- src/views/admin/html/editUniverso.php
+++ src/views/admin/html/editUniverso.php
@@ -29,18 +29,15 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 align="center" class="modal-title" id="exampleModalLabel">Que deseas editar?</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <a href="" class="btn btn-secondary col-12">Editar imagen</a>
+        <a href="" class="btn btn-secondary col-12 mt-3">Editar nombre</a>
       </div>
     </div>
   </div>
